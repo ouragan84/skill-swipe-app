a2296f227420ba30dd08746cbccede2f1f1aca62
Ani Gokul <anigokul@Anis-MacBook-Air-2.local>
2023-03-08 21:47:40 -0800

    minor

--- screens/consumer/register/user/NameDOB.js
+++ screens/consumer/register/user/NameDOB.js
@@ -12,14 +12,32 @@ function clickMe(){
 }
 
 const NameDOB = (props) => {
+  const [date, setDate] = useState("");
+  const handleText = (text) => {
+    if (text.length > 10) return text.substring(0,10)
+    console.log(text)
+    let data = text.split("").filter(i => i !== '/')
+    if (data.length === 3){
+      return `${text.substring(0, 2)}/${text.substring(2)}`
+    }
+    if (data.length === 5){
+      return `${text.substring(0, 5)}/${text.substring(5)}`
+    }
+    return text
+  }
 
   return (
     <SafeAreaView style={{flex:1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{fontSize:moderateScale(32), fontWeight:'bold', paddingBottom:50}}>Tell us your name</Text>
       <InputM name="First Name" placeholder="Enter your first name" />
-      <InputM name="Last Name" placeholder="Enter your first name" />
+      <InputM name="Last Name" placeholder="Enter your last name" />
+      <InputM name="Date of Birth" placeholder="Enter your date of birth" onChangeValue={(text) => {
+        setDate(
+          handleText(text)
+        );
+      }} value={date} numeric={true} />
       <View style={{paddingBottom:moderateScale(50)}}/>
-
+      
       {/* TODO CLICK THIS BUTTON TO ALLOW LOCATION SERVICES */}
       <ButtonM name="Confirm" click={() => props.navigation.navigate('Experience')} />
     </SafeAreaView>
